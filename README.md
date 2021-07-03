bcd0843dd69faabbc8ae8a37e1ca62e2a9d82e5d
Siddharth J Singh <SiddharthJSingh@protonmail.com>
2021-07-03 18:52:24 +0530

    Implemented basic change, make it robust

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # random-wallpaper
 Cycle through wallpapers from configurable source(s)
+
+Added Windows 10 support
--- src/os/implementation.cc
+++ src/os/implementation.cc
@@ -6,7 +6,9 @@ UtilImpl &OSUtils::_impl = *(new Linux());
 #endif
 
 // MSVC
-#ifdef _WIN64 || _WIN32
+#if defined _WIN64 || defined _WIN32
+#include "curl/curl.h"
+const char* Windows::tmpDownload = "D:/test.jpg";
 UtilImpl &OSUtils::_impl = *(new Windows());
 #endif
 
@@ -37,7 +39,42 @@ int Windows::systemCall(const char *command) {
 }
 
 void Windows::updateWallpaper(const string& uri) {
-	stringstream command;
-	command << "feh --bg-fill " << uri;
-	systemCall(command.str().c_str());
+	/* take test.ps1 as a guide and work it up */
+	/* download the file first and let this script transcode the image */
+  CURL *curl_handle;
+  CURLcode retVal;
+  curl_handle = curl_easy_init();
+
+  if (curl_handle) {
+    LogOutput("Handle Initialized...");
+		unique_ptr<ofstream> outfile = make_unique<ofstream>(tmpDownload, std::ios_base::binary);
+    curl_easy_setopt(curl_handle, CURLOPT_URL, uri.c_str());
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION,
+                     &Windows::write_callback);
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, outfile.get());
+
+    retVal = curl_easy_perform(curl_handle);
+
+    curl_easy_cleanup(curl_handle);
+
+    if (retVal == CURLcode::CURLE_OK) {
+			LogOutput("File written...");
+			outfile->close();
+    } else {
+      LogError(curl_easy_strerror(retVal));
+    }
+  } else {
+		LogError("Couldn't obtain handle...");
+	}
+
+	stringstream itemProp;
+	itemProp << "powershell.exe Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name Wallpaper -value \"" << tmpDownload << "\"";
+	systemCall(itemProp.str().c_str());
+	systemCall("powershell.exe rundll32.exe user32.dll, UpdatePerUserSystemParameters");
+}
+
+size_t Windows::write_callback(char* buffer, size_t size, size_t nitems, ofstream* outstream) {
+	//TODO: error handling
+	outstream->write(buffer, nitems);
+	return nitems;
 }

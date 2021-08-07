20feec6232eaf728306bc26a9e7321e9e0b1e49e
Siddharth J Singh <siddharthjsingh@protonmail.com>
2021-08-08 01:54:17 +0530

    Fixed #53

--- src/os/interface.h
+++ src/os/interface.h
@@ -21,55 +21,47 @@ using std::vector;
 
 using std::filesystem::absolute;
 using std::filesystem::create_directories;
+using std::filesystem::directory_options;
 using std::filesystem::exists;
 using std::filesystem::path;
+using std::filesystem::recursive_directory_iterator;
 
 using std::regex;
 using std::regex_replace;
 using std::regex_constants::format_first_only;
 
-class access_denied : std::exception
-{
+class access_denied : std::exception {
   virtual const char *what() const noexcept override;
 };
 
-class UtilImpl
-{
+class UtilImpl {
 public:
   virtual ~UtilImpl(){};
-  /* Provide a relative/absolute path here */
-  virtual vector<string> fetchFiles(const path &dir) = 0;
   virtual int systemCall(const string &command);
   virtual void updateWallpaper(const string &uri) = 0;
 };
 
-class OSUtils
-{
+class OSUtils {
   static UtilImpl &_impl;
 
 public:
   ~OSUtils() { delete &_impl; }
-  static vector<string> fetchFiles(const path &dir)
-  {
-    return _impl.fetchFiles(replaceHome(dir));
-  }
-  static void updateWallpaper(const string &uri) { _impl.updateWallpaper(uri); }
-  /* converts shell ~ to absolute home path */
+  static vector<string> fetchFiles(const path &dir);
+  /* converts shell ~ to absolute home path.
+   * Can be used with windows as well.
+   */
   static const path replaceHome(const path sPath);
+  static void updateWallpaper(const string &uri) { _impl.updateWallpaper(uri); }
 };
 
-class Linux : public UtilImpl
-{
+class Linux : public UtilImpl {
   static const char *tmpWorkDir;
   virtual ~Linux() override;
-  virtual vector<string> fetchFiles(const path &dir) override;
   virtual void updateWallpaper(const string &uri) override;
 };
 
-class Windows : public UtilImpl
-{
+class Windows : public UtilImpl {
   static const char *appDataLoc;
   virtual ~Windows() override;
-  virtual vector<string> fetchFiles(const path &dir) override;
   virtual void updateWallpaper(const string &uri) override;
 };
